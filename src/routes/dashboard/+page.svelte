405e6ed943e358a4be9f6e7985b9df9e4fc25d99
Amit Kumar <amitk@gluelabs.com>
2023-07-19 11:29:34 +0530

    responsive

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,29 +4,29 @@
 	import SideBar from "../../components/SideBar.svelte";
 </script>
 
-<div class="flex flex-row w-screen h-screen">
+<div class="flex flex-col md:flex-row w-screen h-screen">
     <SideBar />
     <div class="w-full h-full flex flex-col bg-neutral-100">
         <div
             class="flex flex-row items-center w-full h-[70px] border-b-2 overflow-hidden bg-slate-100"
         >
-            <div class="w-[60%] flex justify-end items-center">
-                <input
-                    class="border-2 placeholder:text-lg rounded-xl ring-1 my-2 py-2 px-8 mr-2 outline-sky-500"
-                    placeholder="Search Employee"
-                    type="text"
-                />
-                <svg
-                    class="w-8 h-8 fill-slate-400"
-                    focusable="false"
-                    aria-hidden="true"
-                    viewBox="0 0 24 24"
-                    data-testid="SearchIcon"
-                    ><path
-                        d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
-                    /></svg
-                >
-            </div>
+        <div class="w-[60%] flex justify-end items-center">
+            <input
+                class="border-2 placeholder:text-lg rounded-xl ring-1 my-2 py-2 px-8 mr-2 outline-sky-500"
+                placeholder="Search Employee"
+                type="text"
+            />
+            <svg
+                class="w-8 h-8 fill-slate-400 hidden md:block"
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                data-testid="SearchIcon"
+                ><path
+                    d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+                /></svg
+            >
+        </div>
             <div class="w-[40%] flex justify-end mr-5">
                 <button
                     class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
@@ -40,32 +40,22 @@
                 <div class="font-semibold text-4xl ml-5 mt-5">
                     Welcome Admin
                 </div>
-                <div class="w-full h-[25%] flex flex-row items-center">
-                    <div
-                        class="flex flex-col justify-center items-center w-56 h-[50%] bg-gray-600 ml-5 rounded-lg overflow-hidden"
-                    >
-                        <p class="text-5xl text-white">40</p>
-                        <p class="text-white text-xl font-sans">
-                            Total Employees
-                        </p>
+                <div class="w-full h-[25%] flex flex-col md:flex-row md:items-center">
+                    <div class="flex flex-col justify-center items-center w-56 h-[50%] bg-gray-600 ml-5 mb-5 md:mb-0 md:mr-5 rounded-lg overflow-hidden">
+                      <p class="text-5xl md:text-3xl text-white">40</p>
+                      <p class="text-white text-xl md:text-lg font-sans">Total Employees</p>
                     </div>
-                    <div
-                        class="w-56 h-[50%] flex flex-col justify-center items-center bg-green-700 ml-10 rounded-lg overflow-hidden"
-                    >
-                        <p class="text-5xl text-white">30</p>
-                        <p class="text-white text-xl font-sans">On Time</p>
+                    <div class="w-56 h-[50%] flex flex-col justify-center items-center bg-green-700 ml-5 mb-5 md:mb-0 md:ml-10 rounded-lg overflow-hidden">
+                      <p class="text-5xl md:text-3xl text-white">30</p>
+                      <p class="text-white text-xl md:text-lg font-sans">On Time</p>
                     </div>
-                    <div
-                        class="w-56 h-[50%] flex flex-col justify-center items-center bg-red-500 ml-10 rounded-lg overflow-hidden"
-                    >
-                        <p class="text-5xl text-white">7</p>
-                        <p class="text-white text-xl font-sans">Late</p>
+                    <div class="w-56 h-[50%] flex flex-col justify-center items-center bg-red-500 ml-5 mb-5 md:mb-0 md:ml-10 rounded-lg overflow-hidden">
+                      <p class="text-5xl md:text-3xl text-white">7</p>
+                      <p class="text-white text-xl md:text-lg font-sans">Late</p>
                     </div>
-                    <div
-                        class="w-56 h-[50%] flex flex-col justify-center items-center bg-yellow-500 ml-10 mr-5 rounded-lg overflow-hidden"
-                    >
-                        <p class="text-5xl text-white">04</p>
-                        <p class="text-white text-xl font-sans">Absent</p>
+                    <div class="w-56 h-[50%] flex flex-col justify-center items-center bg-yellow-500 ml-5 md:ml-10 rounded-lg overflow-hidden">
+                      <p class="text-5xl md:text-3xl text-white">04</p>
+                      <p class="text-white text-xl md:text-lg font-sans">Absent</p>
                     </div>
                 </div>
                 <div class="w-full h-[70%] flex flex-row">
--- src/routes/employee/+page.svelte
+++ src/routes/employee/+page.svelte
@@ -1,40 +1,34 @@
 <script>
-    import { Card, MenuButton, Dropdown, DropdownItem, Avatar, Button } from "flowbite-svelte";
-	import SideBar from "../../components/SideBar.svelte";
+	import { Card, MenuButton, Dropdown, DropdownItem, Avatar, Button } from 'flowbite-svelte';
+	import SideBar from '../../components/SideBar.svelte';
 </script>
 
 <div class="flex flex-row w-screen h-screen">
-    <SideBar />
-    <div class="w-full h-full flex flex-col bg-neutral-100">
-        <div
-            class="flex flex-row items-center w-full h-[70px] border-b-2 overflow-hidden bg-slate-100"
-        >
-            <div class="w-[60%] flex justify-end items-center">
-                <input
-                    class="border-2 placeholder:text-lg rounded-xl ring-1 my-2 py-2 px-8 mr-2 outline-sky-500"
-                    placeholder="Search Employee"
-                    type="text"
-                />
-                <svg
-                    class="w-8 h-8 fill-slate-400"
-                    focusable="false"
-                    aria-hidden="true"
-                    viewBox="0 0 24 24"
-                    data-testid="SearchIcon"
-                    ><path
-                        d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
-                    /></svg
-                >
-            </div>
-            <div class="w-[40%] flex justify-end mr-5">
-                <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-                >
-                    Logout
-                </button>
-            </div>
-        </div>
-        <div class="w-full h-full flex flex-col">
+	<SideBar />
+	<div class="w-full h-full flex flex-col bg-neutral-100">
+		<div class="flex flex-row items-center w-full h-[70px] border-b-2 overflow-hidden bg-slate-100">
+			<div class="w-[60%] flex justify-end items-center">
+				<input
+					class="border-2 placeholder:text-lg rounded-xl ring-1 my-2 py-2 px-8 mr-2 outline-sky-500"
+					placeholder="Search Employee"
+					type="text"
+				/>
+				<svg
+					class="w-8 h-8 fill-slate-400"
+					focusable="false"
+					aria-hidden="true"
+					viewBox="0 0 24 24"
+					data-testid="SearchIcon"
+					><path
+						d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+					/></svg
+				>
+			</div>
+			<div class="w-[40%] flex justify-end mr-5">
+				<button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"> Logout </button>
+			</div>
+		</div>
+		<!-- <div class="w-full h-full flex flex-col">
             <div class="w-full h-full flex flex-row">
                 <div class="font-semibold text-4xl ml-5 mt-5">
                     32 Employee
@@ -62,6 +56,29 @@
                 </div>
               </Card>
             </div>
-        </div>
-    </div>
+        </div> -->
+		<div class="w-full h-full flex flex-col items-start justify-start">
+			<div class="w-full h-auto flex flex-row items-start mt-7">
+				<div class="font-semibold text-4xl ml-5">32 Employee</div>
+				<button
+					class="ml-auto mr-5 bg-slate-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+					>+New Candidate</button
+				>
+			</div>
+			<div class="ml-5 mt-10">
+				<div class="flex flex-col items-center justify-center w-80 h-auto bg-white">
+					<img
+						src="https://media.istockphoto.com/id/1009749608/photo/young-woman-portrait-in-the-city.jpg?s=612x612&w=0&k=20&c=nBmdXrCkI6Zz-J1EkCGQAamaYlZeIOQAtKunC4WMT-U="
+						alt="Imag"
+						class="w-28 h-28 rounded-full object-cover mt-5"
+					/>
+					<h1 class="text-xl font-semibold mt-4">John Doe</h1>
+					<p class="text-gray-500">Graphic Designer</p>
+                    <div class="w-12 h-12 overflow-hidden">
+
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
